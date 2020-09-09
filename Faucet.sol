def7287218e1ca301029f1e7409109db4e92d19f
Victor Feijo <victor.feijoa@gmail.com>
2020-09-09 09:36:41 +0800

    First contract deploy a Faceut

---
+++ Faucet.sol
@@ -0,0 +1,11 @@
+// First solidyt contract
+contract Faucet {
+  // Receive any money
+  receive () external payable {}
+
+  function withdraw(uint withdraw_amount) public {
+    require(withdraw_amount <= 100000000000000000);
+
+    msg.sender.transfer(withdraw_amount);
+  }
+}
